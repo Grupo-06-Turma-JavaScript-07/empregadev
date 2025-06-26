ec7e2fec76a2272272103059cbeeb2fb19ce8b90
Juliana <juhfreddi@gmail.com>
2025-06-26 13:47:06 -0300

    Atualizações service

---
+++ src/cadastro/service/cadastro.service.ts
@@ -0,0 +1,64 @@
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
+import { Cadastro } from "../entities/cadastro.entity";
+
+@Injectable()
+export class CadastroService {
+    constructor(
+        @InjectRepository(Cadastro)
+        private cadastroRepository: Repository<Cadastro>
+    ) { }
+
+    async findAll(): Promise<Cadastro[]> {
+        return await this.cadastroRepository.find();
+    }
+
+    async findById(id: number): Promise<Cadastro> {
+
+        let cadastro = await this.cadastroRepository.findOne({
+            where: {
+                id
+            }
+        });
+
+        if (!cadastro)
+            throw new HttpException('Cadastro não encontrado!', HttpStatus.NOT_FOUND);
+
+        return cadastro;
+    }
+
+    async findByCargo(titulo: string): Promise<Cadastro[]> {
+        return await this.cadastroRepository.find({
+            where:{
+                titulo: ILike(`%${cargo}%`)
+            }
+        })
+    }
+
+    async create(cadastro: Cadastro): Promise<Cadastro> {
+        return await this.cadastroRepository.save(cadastro);
+    }
+
+    async update(cadastro: Cadastro): Promise<Cadastro> {
+        
+        let buscaCadastro = await this.findById(cadastro.id);
+
+        if (!buscaCadastro || !cadastro.id)
+            throw new HttpException('Cadastro não encontrado!', HttpStatus.NOT_FOUND);
+        
+        return await this.cadastroRepository.save(cadastro);
+    }
+
+    async delete(id: number): Promise<DeleteResult> {
+        
+        let buscaCadastro = await this.findById(id);
+
+        if (!buscaCadastro)
+            throw new HttpException('Cadastro não encontrado!', HttpStatus.NOT_FOUND);
+
+        return await this.cadastroRepository.delete(id);
+
+    }
+
+}
