cbbeee8dc413173a1b55d2851acb632f234139e9
Juliana <juhfreddi@gmail.com>
2025-06-26 11:25:31 -0300

    Module: app.module e cadastro.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CadastroModule } from './cadastro/cadastro.module';
+import { Cadastro } from '.cadastro/entities/cadastro.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_empregadev',
+      entities: [Cadastro],
+      synchronize: true,
+    }),
+    CadastroModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/cadastro/cadastro.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CadastroController } from "./controllers/cadastro.controller"
+import { Cadastro } from ".entities/cadastro.entity";
+import { CadastroService } from "./services/cadastro.service";
+
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Cadastro])],
+    providers: [CadastroService],
+    controllers: [CadastroController],
+    exports: [TypeOrmModule],
+})
+export class CadastroModule {}
